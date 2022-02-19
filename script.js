f2a8ecad8c9c9a827657bf88e443828a9288355d
dirkschumacher <kuemmel.dss@gmx.de>
2022-02-19 22:55:09 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const _init = function() {
+
+    let hiddenImprint = true;
+
+    const linkImprint = document.getElementById('aImprint');
+
+    linkImprint.addEventListener('click', (event) => {
+
+        console.log('clicked', event);
+
+        hiddenImprint = !hiddenImprint;
+
+        const displayValue = hiddenImprint ? 'none' : 'inline';
+        document.getElementById('imprint_content')
+            .style.display = displayValue;
+
+    });
+};
+
+
+const script = function() {
+
+    return {
+
+        init() {
+            _init();
+            return this;
+        },
+
+        hw() {
+            console.log('hallowelt!');
+            return 'hallo welt!'
+        }
+    };
+
+}();
